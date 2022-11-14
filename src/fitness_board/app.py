0d3f8e9c7cd3b7f87da2ec9b56f699bf8d5b7b55
Boian Tzonev <boiantz@gmail.com>
2022-11-14 13:55:54 +0200

    Calc fitenss, fatigue etc.

--- src/fitness_board/app.py
+++ src/fitness_board/app.py
@@ -1,10 +1,21 @@
 import streamlit as st
 import duckdb
+import math
+import pandas as pd
+from dateutil.relativedelta import relativedelta
+import datetime
 
 st.set_page_config(layout='wide')
 db_con = duckdb.connect(database='data/activities.duckdb', read_only=True)
 
-st.write("""# Sport Dashboard""")
+general = st.expander(label="Fitness", expanded=True)
+col1, col2 = general.columns(2)
+period_start = col1.date_input(
+    "Start date",
+    datetime.date(2019, 7, 6))
+period_end = col2.date_input(
+    "End date",
+    datetime.datetime.now())
 
 
 def activities():
@@ -12,22 +23,50 @@ def activities():
 
 
 def heart_rate():
-    query1 = """SELECT date_trunc('day', datetime) as datetime, avg(heart_rate_bpm) as heart_rate_bpm 
-                from activity_records 
+    query1 = """SELECT date_trunc('day', datetime) as datetime, avg(heart_rate_bpm) as heart_rate_bpm
+                from activity_records
                 where datetime between '2022-01-01' and '2022-12-31'
                 group by 1 order by 1"""
     return db_con.execute(query1).df()
 #    return db_con.execute("SELECT heart_rate_bpm, datetime from activity_records ORDER BY datetime LIMIT 1000").df()
 
 
-def fitness():
-    trimp = """select heart_rate_bpm, date_trunc('day', datetime), count(datetime) / 60.0
+def trimp():
+    trimp = f"""
+    select sum(duration * (heart_rate_bpm - 35)/(194-35) * 0.64* 2.718281828459^((heart_rate_bpm - 35)/(194-35))) as trimp, day
+    from
+    (select heart_rate_bpm, date_trunc('day', datetime) as day, count(datetime) / 60.0 as duration
              from activity_records
-             where heart_rate_bpm > 0 
-             and datetime between '2022-01-01' and '2022-12-31'
-             group by 1, 2"""
-    return db_con.execute(trimp).df()
+             where heart_rate_bpm > 35
+             and datetime between '{period_start}' and '{period_end}'
+             group by 1, 2)
+    group by 2
+    order by day"""
+    return db_con.execute(trimp).df().set_index('day').to_dict()
 
 
-st.dataframe(fitness(), use_container_width=True)
-st.dataframe(activities(), use_container_width=True)
+def banister():
+    trimp_data = trimp()
+    fitness = 0.0
+    fatigue = 0.0
+    r1 = 50
+    r2 = 11
+    k1 = 1.0
+    k2 = 2.0
+    result = []
+
+    daterange = pd.date_range(
+        period_start, period_end + relativedelta(weeks=+2))
+    for date in daterange:
+        fitness = fitness * math.exp(-1/r1) + trimp_data['trimp'].get(date, 0)
+        fatigue = fatigue * math.exp(-1/r2) + trimp_data['trimp'].get(date, 0)
+        performance = fitness * k1 - fatigue * k2
+        result.append((fitness,
+                       fatigue, performance,
+                       date))
+
+    return pd.DataFrame(result, columns=['fitness', 'fatigue', 'form', 'date'])
+
+
+general.line_chart(banister(), x='date', use_container_width=True)
+st.dataframe(banister(), use_container_width=True)
